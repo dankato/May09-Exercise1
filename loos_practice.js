0703dd07b36d10db4cb0aefabd5970b2585ac88d
PROB8 <blackb8r@gmail.com>
2017-05-09 12:13:45 -0400

    update with average drill

--- loos_practice.js
+++ loos_practice.js
@@ -1,3 +1,5 @@
+ //max and min drill, using while loops
+
    		function max(numbers) {
 			var currentMax = numbers[0];
       var i=0;
@@ -21,3 +23,30 @@
         }
       	i++;
       }
+
+
+//compute the avg drill using while loops
+
+function average(numbers) {
+  var total = numbers[0];
+  var i = 1;
+  while (i < numbers.length) {
+    total += numbers[i];
+    i++;
+  }
+  return total / numbers.length;
+}
+average([4, 6, 8, 10, 12, ]);
+
+function average(numbers) {
+  var total = numbers[0];
+  var i = 1;
+
+  while (i < numbers.length) {
+    total += numbers[i];
+    i++;
+  }
+  let avg = total / numbers.length;
+  return avg;
+}
+average([4, 6, 8, 10, 12])
